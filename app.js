9723f254b15e6a608ba8b30510f13bd801760bc4
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-19 11:13:38 +0600

    database connection done

--- app.js
+++ app.js
@@ -4,14 +4,36 @@ require('dotenv').config()
 
 const app = express();
 
+// Middleware
+// for hanling request data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+// MongoDB Connection String
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tamdy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const port = process.env.PORT;
 
 
+// Home Router
 app.get('/', (req, res) => {
     res.json({
         message: 'welcome'
     });
 });
-const port = process.env.PORT;
-app.listen(port || 4000, () => {
-    console.log("Server Listening...");
-});
+
+// Connect with Mongoose
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        app.listen(port || 4000, () => {
+            console.log(`Server is Listening at ${port}`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.json({
+            error: "Mongo Error Occured!"
+        })
+    })
